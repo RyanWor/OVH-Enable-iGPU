9431473e4bba64aef7ed4c889b150def740ab137
Ryan Worrell <ryanworrell@gmail.com>
2021-12-03 10:47:25 -0600

    Add /opt fix
    Reformats /opt (/dev/md4) as btrfs and updates /etc/fstab accordingly (includes unmounting, remounting, and backups of both new and old fstab in /root)

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,15 @@
 !/bin/bash
+cp /etc/fstab /root/fstab.old
+UUIDOPTOLD="$(sudo blkid -s UUID -o value /dev/md4$partnumber)"
+umount /dev/md4
+mkfs.btrfs -f /dev/md4
+UUIDOPTNEW="$(sudo blkid -s UUID -o value /dev/md4$partnumber)"
+awk '$2~"^/opt$"{$3="btrfs"}1' OFS="\t" /root/fstab.old | tee -a /root/fstab.tmp
+sed "s/^UUID=$UUIDOPTOLD/UUID=$UUIDOPTNEW/" </root/fstab.tmp | tee -a /root/fstab.new
+rm /root/fstab.tmp
+cp /root/fstab /etc/fstab
+chmod 644 /etc/fstab
+mount -a
 apt update && apt dist-upgrade -y
 sed -e s/\ nomodeset//g -i /etc/default/grub
 update-grub
